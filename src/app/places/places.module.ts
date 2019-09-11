bfb895293558ea3ae2cad1eef73f5891c71f58b7
reuelrds <reuelrds@gmail.com>
2019-09-12 00:06:54 +0530

    Added Tabs and its routing strategy
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { PlacesPage } from './places.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  }
-];
+import { PlacesRoutingModule } from './places-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    PlacesRoutingModule
   ],
   declarations: [PlacesPage]
 })
