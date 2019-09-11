a4963a25d1ea29f96c260211fce7f826e4fe197d
reuelrds <reuelrds@gmail.com>
2019-09-12 01:05:11 +0530

    _

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,21 @@ export class PlacesService {
       'p1',
       'Manhattan Mansion',
       'In the heart of New York City',
-      '',
+      'https://tinyurl.com/yxptpg42',
       149.99
     ),
     new Place(
       'p2',
       'L\'Amour Toujours',
       'A Romantic Place in Paris!',
-      '',
+      'https://tinyurl.com/y2ww9a5b',
       189.99
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
-      '',
+      'https://tinyurl.com/yx8p7d6a',
       99.99
     )
   ];
