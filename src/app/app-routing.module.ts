ab643fd406ed2cb575a862330ffae2a904bfad25
reuelrds <reuelrds@gmail.com>
2019-09-11 16:58:29 +0530

    Generated Initial App Pages
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', loadChildren: './auth/auth.module#AuthPageModule'},
+  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
+  { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
+  { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
+  { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
+  { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
+  { path: 'offer-bookings', loadChildren: './places/offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule' },
+  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' }
+];
 
 @NgModule({
   imports: [
