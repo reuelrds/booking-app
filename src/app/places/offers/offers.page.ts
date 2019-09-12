564e4b56fff64dcca0c52afb2735929ef22c18ba
reuelrds <reuelrds@gmail.com>
2019-09-13 01:10:57 +0530

    Displayed dummy data on offers page and added navigation to go back and forth to offer-bookings page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
+
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersPage implements OnInit {
 
-  constructor() { }
+  offers: Place[];
+
+  constructor(
+    private placesService: PlacesService
+  ) { }
 
   ngOnInit() {
+    this.offers = this.placesService.get_places();
   }
 
 }
