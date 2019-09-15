9f89439aa9ba6038d552df2e9474543a83b10572
reuelrds <reuelrds@gmail.com>
2019-09-15 23:07:03 +0530

    Extracted place-id from url in place-detial and edit-offer pages and added back button to edit-offer page
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { NavController } from '@ionic/angular';
+
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,11 +13,25 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
+  place: Place;
+
   constructor(
-    private navController: NavController
+    private navController: NavController,
+    private activatedRoute: ActivatedRoute,
+    private placesService: PlacesService
   ) { }
 
   ngOnInit() {
+
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+
+      if (!paramMap.has('placeId')) {
+        this.navController.navigateBack('/offers/tabs/discover');
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   onBookPlace() {
--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { NavController } from '@ionic/angular';
+
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  place: Place;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private navController: NavController,
+    private placesService: PlacesService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navController.navigateBack('/places/tabs/offers');
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+
+    });
   }
 
 }
