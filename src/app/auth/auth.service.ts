0fbd3748c1bfdd214b319ead287a2a2c3e203c6d
reuelrds <reuelrds@gmail.com>
2019-09-17 20:27:29 +0530

    _

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  private _userIsAuthenticated = false;
+  private _userIsAuthenticated = true;
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
