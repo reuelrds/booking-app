1c32ff84b561d73563c67696430ab6260e18bbcd
reuelrds <reuelrds@gmail.com>
2019-10-06 13:19:59 +0530

    Added virtual scroll to the listed places
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -10,11 +10,13 @@ import { Place } from '../place.model';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
-    this.loadedPlaces = this.placesService.get_places();
+    this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
 
   }
 
