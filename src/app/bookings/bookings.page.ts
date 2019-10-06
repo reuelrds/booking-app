453390b7d9035df4f969e64494c0b09f63362bf4
reuelrds <reuelrds@gmail.com>
2019-10-06 12:53:14 +0530

    Display a dummy swipeable booking on the bookings page

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IonItemSliding } from '@ionic/angular';
+
+import { BookingService } from './booking.service';
+import { Booking } from './booking.model';
+
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsPage implements OnInit {
 
-  constructor() { }
+  loadedBookings: Booking[];
+
+  constructor(
+    private bookingService: BookingService
+  ) { }
 
   ngOnInit() {
+    this.loadedBookings = this.bookingService.bookings;
+  }
+
+  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+    slidingEl.close();
+    // Cancel booking with id offerId
   }
 
 }
