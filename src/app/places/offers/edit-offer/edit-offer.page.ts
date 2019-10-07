3aa820158b4dd88ecf37835b53e191750c2c97f4
reuelrds <reuelrds@gmail.com>
2019-10-07 15:17:15 +0530

    Added Edit Offer Form template
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { NavController } from '@ionic/angular';
 
@@ -9,17 +10,18 @@ import { PlacesService } from '../../places.service';
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  styleUrls: ['./edit-offer.page.scss']
 })
 export class EditOfferPage implements OnInit {
-
   place: Place;
+  form: FormGroup;
+  descriptionLength = 0;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private navController: NavController,
     private placesService: PlacesService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -29,8 +31,29 @@ export class EditOfferPage implements OnInit {
       }
 
       this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
 
+      this.descriptionLength = this.place.description.length;
     });
   }
 
+  onKeyPress(description: string) {
+    this.descriptionLength = description.length;
+  }
+
+  onUpdateOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form);
+  }
 }
