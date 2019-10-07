95078b7dd72d4d963de75f1b9d69cd9279b1ef2a
reuelrds <reuelrds@gmail.com>
2019-10-07 21:29:05 +0530

    Updated to show correct Dates
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/places/offers/offer-item/offer-item.component.ts
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -5,18 +5,12 @@ import { Place } from '../../place.model';
 @Component({
   selector: 'app-offer-item',
   templateUrl: './offer-item.component.html',
-  styleUrls: ['./offer-item.component.scss'],
+  styleUrls: ['./offer-item.component.scss']
 })
 export class OfferItemComponent implements OnInit {
-
   @Input() offer: Place;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
-  getDummyDate() {
-    return new Date();
-  }
-
 }
