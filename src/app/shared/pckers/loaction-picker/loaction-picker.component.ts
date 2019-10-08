7786106c33f71b53d7f758b05521b7a34327f258
reuelrds <reuelrds@gmail.com>
2019-10-09 04:43:05 +0530

    Get address of the picked place
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/shared/pckers/loaction-picker/loaction-picker.component.ts
+++ src/app/shared/pckers/loaction-picker/loaction-picker.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
 
 import { ModalController } from '@ionic/angular';
 
+import { environment } from '../../../../environments/environment';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 
 @Component({
@@ -10,7 +14,10 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
   styleUrls: ['./loaction-picker.component.scss']
 })
 export class LoactionPickerComponent implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit() {}
 
@@ -21,9 +28,31 @@ export class LoactionPickerComponent implements OnInit {
       })
       .then(modalEl => {
         modalEl.onDidDismiss().then(modalData => {
-          console.log(modalData.data);
+          if (!modalData.data) {
+            return;
+          }
+          this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
+            address => {
+              console.log(address);
+            }
+          );
         });
         modalEl.present();
       });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.httpClient
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`
+      )
+      .pipe(
+        map(resData => {
+          if (!resData || !resData.results || resData.results.length === 0) {
+            return null;
+          }
+          return resData.results[0].formatted_address;
+        })
+      );
   }
 }
