59207d7858b7b3d3b92d26784599218e826744ca
reuelrds <reuelrds@gmail.com>
2019-10-09 13:06:08 +0530

    Removed Google Maps API Key
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyDyAkQjiExTcnWbkFBL2yWUh2W0fWshXpM'
+  googleMapsAPIKey: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  googleMapsAPIKey: 'AIzaSyDyAkQjiExTcnWbkFBL2yWUh2W0fWshXpM'
+  googleMapsAPIKey: ''
 };
 
 /*
