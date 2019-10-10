0ccd3991e9434165e51d3973c89d90e8699e8ed5
reuelrds <reuelrds@gmail.com>
2019-10-10 12:35:20 +0530

    Added a Falback if a camera is not available
    Signed-off-by: reuelrds <reuelrds@gmail.com>

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 import {
   Plugins,
@@ -6,6 +13,7 @@ import {
   CameraSource,
   CameraResultType
 } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -13,17 +21,30 @@ import {
   styleUrls: ['./image-picker.component.scss']
 })
 export class ImagePickerComponent implements OnInit {
-
   @Output() imagePick = new EventEmitter<string>();
 
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<
+    HTMLInputElement
+  >;
+
   selectedImage: string;
+  usePicker = false;
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (
+      this.platform.is('mobile') ||
+      this.platform.is('hybrid') ||
+      this.platform.is('desktop')
+    ) {
+      this.usePicker = true;
+    }
+  }
 
   onPickImage() {
-    if (!Capacitor.isPluginAvailable('Camera')) {
+    if (!Capacitor.isPluginAvailable('Camera') || this.usePicker) {
+      this.filePickerRef.nativeElement.click();
       return;
     }
     Plugins.Camera.getPhoto({
@@ -32,12 +53,27 @@ export class ImagePickerComponent implements OnInit {
       correctOrientation: true,
       width: 600,
       resultType: CameraResultType.DataUrl
-    }).then(image => {
-      this.selectedImage = image.dataUrl;
-      this.imagePick.emit(this.selectedImage);
-    }).catch(error => {
-      console.log(error);
-      return false;
-    });
+    })
+      .then(image => {
+        this.selectedImage = image.dataUrl;
+        this.imagePick.emit(this.selectedImage);
+      })
+      .catch(error => {
+        console.log(error);
+        return false;
+      });
+  }
+
+  onFileChosen(event: Event) {
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+    const fr = new FileReader();
+    fr.onload = () => {
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+    };
+    fr.readAsDataURL(pickedFile);
   }
 }
