cde84a80e9e4de41fc5617578239d13e30e90a1b
reuelrds <reuelrds@gmail.com>
2019-10-24 00:08:46 +0530

    Shifted handler functions to places controller

--- backend/src/routes/places.php
+++ backend/src/routes/places.php
@@ -4,13 +4,11 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
-$placesHandler = function (Request $request, Response $response) {
-	$response->getBody()->write("Places are here");
-	return $response;
-};
+$placesHandler = require __DIR__ . '/../controllers/places/places.controller.php';
 
 $placesRoutes = function (Group $group) use ($placesHandler) {
-	$group->get('', $placesHandler);
+  $group->get('', $placesHandler["getPlaces"]);
+  $group->get('/post', $placesHandler["postPlaces"]);
 };
 
 return $placesRoutes;
